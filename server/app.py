1b9cce3f4887f9638456024ea540381c5d10b987
Denis <omelched@gmail.com>
2020-05-03 14:55:43 +0300

    документация функций

--- server/app.py
+++ server/app.py
@@ -11,7 +11,19 @@ HOST, PORT = 'localhost', 15151
 
 
 class ServerApp(object):
-    def __init__(self, settings):
+    """
+    ServerApp - класс приложений.
+    Экземпляр этого класса является непосредственно приложением, производящим общение с пользователем.
+    """
+    def __init__(self, settings: tuple):
+        """
+        Метод инициализации.
+        Сохраняет в память на каком сервере и порте запускается.
+        Инстанциирует класс Server.
+        Создает thread перехвата входящих TCP-запросов, но не запускает его.
+
+        :param settings: кортеж в формате (IP: str, PORT: int)
+        """
         self.HOST, self.PORT = settings[0], settings[1]
 
         self.server = Server((HOST, PORT),
@@ -22,6 +34,12 @@ class ServerApp(object):
         self.server_thread.daemon = True
 
     def interface_loop(self):
+        """
+        Метод запуска интерфейса приложения.
+        Запускает thread перехвата входящих TCP-запросов.
+        Отображает пользователю доступные команды и ждет их ввода.
+        :return:
+        """
         self.server_thread.start()
         print('Server loop running in thread:', self.server_thread.name)
         print("Доступные команды:\n\tlist: — показать список подсоединённых клиентов\n\tq: — выключить программу\n"
