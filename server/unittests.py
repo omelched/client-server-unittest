d982a4978a80269f0dc1b4081ebf1cdc716e13ff
Denis <omelched@gmail.com>
2020-05-10 01:31:57 +0300

    меткие доработки

--- server/unittests.py
+++ server/unittests.py
@@ -36,26 +36,26 @@ class UtilsTests(unittest.TestCase):
 
     def test_get_hash(self):
         self.assertEqual('1c021062b3fbe06635aa91a343e176baec1e4946', utils.get_hash(_SESSION, _VERSION))
-
-
-class ServerClassTests(unittest.TestCase):
-
-    def test_Server_send_message(self):
-        self.test_server_obj = network.Server(('localhost', 8080),
-                                              network.ThreadedTCPRequestHandler,
-                                              network.MessageProcessor,
-                                              network.ServerExecutor)
-
-        test_message = message.OutputMessage((_SESSION, _VERSION, _CHUNK_SIZE), body=('ping_pong', 'test_msg'))
-        listener_server = TestServer(('localhost', 8081), TestRequestHandler)
-        listener_server.server_thread = threading.Thread(target=listener_server.serve_forever)
-        listener_server.server_thread.daemon = True
-        listener_server.server_thread.start()
-        self.test_server_obj.send_message(('localhost', 8081), test_message)
-        time.sleep(.001)
-        self.assertEqual([i for i in test_message.encode()], listener_server.data)
-        self.test_server_obj.server_close()
-        del self.test_server_obj
-        listener_server.server_close()
-        del listener_server
-        time.sleep(.001)
+#
+#
+# class ServerClassTests(unittest.TestCase):
+#
+#     def test_Server_send_message(self):
+#         self.test_server_obj = network.Server(('localhost', 8080),
+#                                               network.ThreadedTCPRequestHandler,
+#                                               network.MessageProcessor,
+#                                               network.ServerExecutor)
+#
+#         test_message = message.OutputMessage((_SESSION, _VERSION, _CHUNK_SIZE), body=('ping_pong', 'test_msg'))
+#         listener_server = TestServer(('localhost', 8081), TestRequestHandler)
+#         listener_server.server_thread = threading.Thread(target=listener_server.serve_forever)
+#         listener_server.server_thread.daemon = True
+#         listener_server.server_thread.start()
+#         self.test_server_obj.send_message(('localhost', 8081), test_message)
+#         time.sleep(.001)
+#         self.assertEqual([i for i in test_message.encode()], listener_server.data)
+#         self.test_server_obj.server_close()
+#         del self.test_server_obj
+#         listener_server.server_close()
+#         del listener_server
+#         time.sleep(.001)
